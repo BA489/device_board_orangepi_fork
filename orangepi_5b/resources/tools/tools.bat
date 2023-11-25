ed515525fd54e2d13f866a2620d53014d92775c4
AlgoIdeas <yu19881234@163.com>
2023-11-25 13:21:20 +0800

    update resources hap

--- orangepi_5b/resources/tools/tools.bat
+++ orangepi_5b/resources/tools/tools.bat
@@ -1,5 +1,7 @@
 hdc shell mount -o remount,rw /
-hdc file send bin/modetest /system/bin
+hdc file send .\bin\modetest /system/bin
 hdc shell chmod 777 /system/bin/modetest
-hdc file send bin/busybox /system/bin
+hdc file send .\bin\busybox /system/bin
 hdc shell chmod 777 /system/bin/busybox
+hdc file send .\bin\v4l2-ctl /system/bin
+hdc shell chmod 777 /system/bin/v4l2-ctl
